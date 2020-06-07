899c58898c45336df1e162902029be26a576bc4e
Davy de Haas <davy.dehaas98@gmail.com>
2020-06-07 19:04:24 +0200

    DWET-84: Remove appsettings.Development.json

--- PostMicroservice/Controllers/PostController.cs
+++ PostMicroservice/Controllers/PostController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Net.Mime;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PostMicroservice.Exceptions;
